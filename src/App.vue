109e04ba29cba08a27ca0da25bb35e0b13877168
Burt <burthuang@foxmail.com>
2022-10-17 15:42:11 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="title">Hello, This is Vue3 Template!</div>
+</template>
+
+<style scoped lang="scss">
+.title {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  color: #007acc;
+}
+</style>
